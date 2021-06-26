af0a43ee4923ec386f41f89c0863d74c27076007
Terence Goldberg <tjg37@cantab.net>
2021-06-26 10:31:02 -0600

    initial changes

--- src/android/com/dinamexoft/googleplayserviceschecker/GooglePlayServicesChecker.java
+++ src/android/com/dinamexoft/googleplayserviceschecker/GooglePlayServicesChecker.java
@@ -3,6 +3,8 @@ package com.dinamexoft.googleplayserviceschecker;
 import android.util.Log;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.huawei.hms.api.ConnectionResult;
+import com.huawei.hms.api.HuaweiApiAvailability;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
@@ -18,20 +20,24 @@ public class GooglePlayServicesChecker extends CordovaPlugin {
         if (action.equals("check")) {
             Log.i(APPTAG, "checkPlayServices");
             JSONObject json = new JSONObject();
-            final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
             GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
-            int result = googleAPI.isGooglePlayServicesAvailable(this.cordova.getContext());
-            if (result != ConnectionResult.SUCCESS) {
-                Log.i(APPTAG, "result != ConnectionResult.SUCCESS");
-                json.put("status", false).put("ConnectionResult", result);
-                if (googleAPI.isUserResolvableError(result)) {
-                    Log.i(APPTAG, "googleAPI.isUserResolvableError(result)");
-                    googleAPI.getErrorDialog(this.cordova.getActivity(), result, PLAY_SERVICES_RESOLUTION_REQUEST).show();
-                    json.put("message", "user action required");
-                }
-            } else {
-                json.put("status", true);
+            int googleResult = googleAPI.isGooglePlayServicesAvailable(this.cordova.getContext());
+            if (googleResult == com.google.android.gms.common.ConnectionResult.SUCCESS) {
+                Log.i(APPTAG, "Google Services Exist");
+                json.put("googleStatus", true);
+            }else{
+              json.put("googleStatus", false);
             }
+
+            HuaweiApiAvailability huaweiAPI = HuaweiApiAvailability.getInstance();
+            int resultHuawei = huaweiAPI.isHuaweiMobileServicesAvailable(this.cordova.getContext());
+            if (resultHuawei == com.huawei.hms.api.HuaweiApiAvailability.SUCCESS) {
+                Log.i(APPTAG, "Huawei Services Exist");
+                json.put("huaweiStatus", true);
+            }else{
+              json.put("huaweiStatus", false);
+            }
+
             callbackContext.success(json);
             return true;
         } else {
